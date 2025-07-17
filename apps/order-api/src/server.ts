81bc32303dfee4e5ea888720146c4978067f7a41
sumozi <oleh.sbef@gmail.com>
2025-07-17 18:27:52 +0200

    Update sequence of startup

--- apps/order-api/src/server.ts
+++ apps/order-api/src/server.ts
@@ -1,8 +1,14 @@
+import { initializeRedisClients } from 'shared/lib/redis';
 import app from './index';
 import logger from 'shared/lib/logger';
+import { getIp } from 'shared/utils/ip';
 
 const PORT = process.env.PORT || 3000;
 
+initializeRedisClients().catch((error) => {
+    logger.error('Redis initialization failed:', { error, ip: getIp() });
+    process.exit(1);
+  }); 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`, { ip: getIp() });
 }); 
